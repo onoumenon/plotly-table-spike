24058888d3cf5636ab0738959e7a8b085d23bec3
huitian <tanhuitian@gmail.com>
2019-04-09 00:38:54 +0800

    Fit Visualization to Model

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Plot from "react-plotly.js";
+import {
+  categories,
+  emailsExplore,
+  emailsDeepen,
+  emailsShare,
+  emailsExploreNumber,
+  emailsDeepenNumber,
+  emailsShareNumber
+} from "./data";
 
-class App extends Component {
+// Bar chart, can try mapping through if route for action plans exist
+const trace1 = {
+  x: emailsExploreNumber,
+  y: categories,
+  text: emailsExplore,
+  name: "Explore",
+  orientation: "h",
+  type: "bar",
+  hoverinfo: "x+y+name"
+};
+
+const trace2 = {
+  x: emailsDeepenNumber,
+  y: categories,
+  text: emailsDeepen,
+  name: "Deepen",
+  orientation: "h",
+  type: "bar",
+  hoverinfo: "x+y+name"
+};
+const trace3 = {
+  x: emailsShareNumber,
+  y: categories,
+  text: emailsShare,
+  name: "Share",
+  orientation: "h",
+  type: "bar",
+  hoverinfo: "x+y+name"
+};
+
+const barData = [trace1, trace2, trace3];
+
+class App extends React.Component {
+  state = {
+    category: "Click a category to get the relevant emails",
+    explore: [],
+    deepen: [],
+    share: []
+  };
+  onClick({ click }) {
+    const category = click.points[0].y;
+    const explore = click.points[0].text;
+    const deepen = click.points[1].text;
+    const share = click.points[2].text;
+
+    this.setState({ category });
+    this.setState({ explore });
+    this.setState({ deepen });
+    this.setState({ share });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Plot
+          data={barData}
+          layout={{
+            title: "Action Plans",
+            barmode: "stack",
+            bargroupgap: 0.1,
+            yaxis: { showgrid: false, automargin: true },
+            xaxis: { zeroline: false, showgrid: false, automargin: true },
+            colorway: ["#75ddbe", "#dfe38e", "#fabc60"]
+          }}
+          onClick={click => this.onClick({ click })}
+          useResizeHandler={true}
+          style={{
+            width: "80%",
+            height: "100%"
+          }}
+        />
+        <Plot
+          data={[
+            {
+              type: "table",
+              header: {
+                values: [
+                  ["<b>CATEGORY</b>"],
+                  ["<b>Explore</b>"],
+                  ["<b>Deepen</b>"],
+                  ["<b>Share</b>"]
+                ],
+                align: ["left", "center"],
+                line: { width: 1, color: "#506784" },
+                fill: { color: "#75ddbe" },
+                font: { family: "Arial", size: 24, color: "white" }
+              },
+              cells: {
+                values: [
+                  this.state.category,
+                  this.state.explore,
+                  this.state.deepen,
+                  this.state.share
+                ],
+                align: ["left", "center"],
+                line: { color: "#506784", width: 1 },
+                height: 30,
+                fill: { color: ["white"] },
+                font: { family: "Arial", size: 18, color: ["#506784"] }
+              }
+            }
+          ]}
+          useResizeHandler={true}
+          style={{ width: "80%", maxHeight: "500%" }}
+        />
+      </>
     );
   }
 }
---
+++ src/data.js
@@ -0,0 +1,270 @@
+export const categories = [
+  "Diversity and Inclusion",
+  "Religious Minorities",
+  "Society and Privilege",
+  "Climate Injustice",
+  "Equitable Tech",
+  "Sexual Orientation and Gender Identity",
+  "Racial Minorities",
+  "Economic Justice"
+];
+
+export const offices = ["Singapore", "Thailand", "Others"];
+export const levels = ["1. Open", "2. Informed", "3. Engaged", "4. Activated"];
+export const actions = [
+  "Would like to explore",
+  "Would like to deepen",
+  "Would like to share"
+];
+
+// individual data
+export const data = [
+  {
+    timeStamp: Date.now(),
+    name: "Leonard Smith",
+    email: "leonard@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [actions[0]] },
+      { category: categories[1], level: levels[3], action: [actions[0]] },
+      { category: categories[2], level: levels[3], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[1]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[1]] },
+      { category: categories[6], level: levels[1], action: [actions[1]] },
+      { category: categories[7], level: levels[3], action: [actions[0]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Feng Liu",
+    email: "fl@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [actions[0]] },
+      { category: categories[1], level: levels[3], action: [actions[0]] },
+      { category: categories[2], level: levels[3], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[1]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[1]] },
+      { category: categories[6], level: levels[1], action: [actions[1]] },
+      { category: categories[7], level: levels[3], action: [actions[0]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Raj",
+    email: "raj@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [] },
+      { category: categories[1], level: levels[3], action: [] },
+      { category: categories[2], level: levels[3], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[1]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[1]] },
+      { category: categories[6], level: levels[1], action: [actions[1]] },
+      { category: categories[7], level: levels[3], action: [actions[0]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Liu Ji",
+    email: "lj@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [] },
+      { category: categories[1], level: levels[3], action: [] },
+      { category: categories[2], level: levels[3], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[3], action: [actions[0]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Rulin",
+    email: "rulin@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [] },
+      { category: categories[1], level: levels[3], action: [] },
+      { category: categories[2], level: levels[3], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[0]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[3], action: [actions[0]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Yipin Chen",
+    email: "ypc@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      { category: categories[0], level: levels[3], action: [actions[2]] },
+      { category: categories[1], level: levels[3], action: [actions[2]] },
+      { category: categories[2], level: levels[3], action: [actions[2]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[2]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[3], action: [actions[2]] }
+    ]
+  },
+  {
+    timeStamp: Date.now(),
+    name: "Nina Sorkin",
+    email: "nina@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      {
+        category: categories[0],
+        level: levels[1],
+        action: [actions[0], actions[1]]
+      },
+      { category: categories[1], level: levels[1], action: [actions[1]] },
+      { category: categories[2], level: levels[0], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[0], action: [] }
+    ]
+  },
+  {
+    time: Date.now(),
+    name: "Anna Pavlova",
+    email: "ap@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      {
+        category: categories[0],
+        level: levels[1],
+        action: [actions[0], actions[1]]
+      },
+      { category: categories[1], level: levels[1], action: [actions[0]] },
+      { category: categories[2], level: levels[0], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[0], action: [actions[0]] }
+    ]
+  },
+  {
+    time: Date.now(),
+    name: "Nellie Melba",
+    email: "nb@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      {
+        category: categories[0],
+        level: levels[1],
+        action: [actions[0], actions[1]]
+      },
+      { category: categories[1], level: levels[1], action: [actions[1]] },
+      { category: categories[2], level: levels[0], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[0], action: [actions[0]] }
+    ]
+  },
+  {
+    time: Date.now(),
+    name: "James Salisbury",
+    email: "js@thoughtworks.com",
+    office: offices[0],
+    categories: [
+      {
+        category: categories[0],
+        level: levels[1],
+        action: [actions[0], actions[1]]
+      },
+      { category: categories[1], level: levels[1], action: [actions[0]] },
+      { category: categories[2], level: levels[0], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[1]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[0], action: [actions[1]] }
+    ]
+  },
+  {
+    time: Date.now(),
+    name: "Kate Chairchua",
+    email: "kc@thoughtworks.com",
+    office: offices[1],
+    categories: [
+      {
+        category: categories[0],
+        level: levels[1],
+        action: [actions[0], actions[1]]
+      },
+      { category: categories[1], level: levels[1], action: [actions[0]] },
+      { category: categories[2], level: levels[0], action: [actions[0]] },
+      { category: categories[3], level: levels[2], action: [actions[0]] },
+      { category: categories[4], level: levels[3], action: [actions[0]] },
+      { category: categories[5], level: levels[3], action: [actions[2]] },
+      { category: categories[6], level: levels[1], action: [actions[0]] },
+      { category: categories[7], level: levels[0], action: [actions[0]] }
+    ]
+  }
+];
+
+const findEmails = (action, category) => {
+  const foundPersons = data.filter(person => {
+    if (
+      person.categories.filter(
+        cat => cat.category === category && cat.action.includes(action)
+      )[0]
+    )
+      return true;
+  });
+  return foundPersons.map(person => person.email);
+};
+
+const findOne = tarName => {
+  const oneDatum = data.find(a => a.name === tarName);
+  return [oneDatum];
+};
+
+const findAllExcept = tarName => {
+  const oneDatum = data.find(a => a.name === tarName);
+  const index = data.indexOf(oneDatum);
+  const copy = data.slice();
+  copy.splice(index, 1);
+  return copy;
+};
+
+export { findOne, findAllExcept };
+
+// emails + no of emails found by action plan, ordered by category
+export const emailsExplore = categories.map(category =>
+  findEmails(actions[0], category)
+);
+
+export const emailsExploreNumber = categories.map(
+  category => findEmails(actions[0], category).length
+);
+
+export const emailsDeepen = categories.map(category =>
+  findEmails(actions[1], category)
+);
+
+export const emailsDeepenNumber = categories.map(
+  category => findEmails(actions[1], category).length
+);
+
+export const emailsShare = categories.map(category =>
+  findEmails(actions[2], category)
+);
+
+export const emailsShareNumber = categories.map(
+  category => findEmails(actions[2], category).length
+);
